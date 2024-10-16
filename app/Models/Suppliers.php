51b291288e3d07fda953b38fad840117d986d7ff
Nurkhankazi <nurkhan7494@gmail.com>
2024-10-17 00:05:42 +0600

    update

--- app/Models/Suppliers.php
+++ app/Models/Suppliers.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class suppliers extends Model
+class Suppliers extends Model
 {
     use HasFactory;
     protected $fillable=['suppliername', 'contactperson', 'phonenumber', 'email ', 'address'];
